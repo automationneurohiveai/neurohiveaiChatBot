0577dfb7f27aeff3932fa386b21930084444293c
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2025-06-28 19:36:33 +0300

    result

--- client/src/App.js
+++ client/src/App.js
@@ -10,39 +10,9 @@ import { useSearchParams } from "react-router-dom";
 function App() {
   const { visible } = useUIContext();
   const [searchParams] = useSearchParams();
-  const [lang, setLang] = useState('en');
 
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const handleMessage = (event) => {
-      console.log("📩 Отримано повідомлення:", event);
-  
-      if (event.origin !== "https://neurohiveai.agency" ) return;
-  
-      try {
-        const data = JSON.parse(event.data);
-        console.log("✅ Розпарсено повідомлення:", data);
-  
-        if (data.type === "set-language" && ['en', 'pl', 'uk'].includes(data.lang)) {
-          console.log("🌐 Мова отримана:", data.lang);
-          setLang(data.lang);
-        }
-      } catch (err) {
-        console.warn("❌ Помилка при парсингу повідомлення:", event.data);
-      }
-    };
-  
-    window.addEventListener("message", handleMessage);
-    return () => window.removeEventListener("message", handleMessage);
-  }, []);
-  
-
-  const normalizedLang = ['en', 'uk', 'pl'].includes(lang) ? lang : 'en';
-
-
-
-
-
+  // Отримуємо значення параметра lang з URL
+  const lang = searchParams.get("lang") || "en"; // fallback до "en"
 
   useEffect(() => {
     (async () => {
@@ -58,7 +28,7 @@ function App() {
     <>
       <div className="App min-h-screen flex items-center justify-center">
         <div className="w-full max-w-4xl">
-          <Intro lang={normalizedLang}/>
+          <Intro lang={lang} />
         </div>
       </div>
 
@@ -68,48 +38,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getLanguageFromUrl() {
-//   const path = window.location.pathname.toLowerCase(); 
-//   if (path.startsWith('/uk/')) {
-//     console.log('Language detected: uk');
-//     return 'uk';
-//   } else if (path.startsWith('/pl/')) {
-//     console.log('Language detected: pl');
-//     return 'pl';
-//   } else {
-//     console.log('Language detected: en (default)');
-//     return 'en';
-//   }
-// }
-
-
-
-
-//   const iframe = document.querySelector('iframe');
-
-//   const lang = getLanguageFromUrl();
-//    const data = {
-//      type: "set-language",
-//      lang: lang
-//    };
-  
-//      iframe.contentWindow.postMessage(
-//        JSON.stringify(data),                 
-//        "https://neurohiveaichatbot.onrender.com"       
-//      );
-   
-
